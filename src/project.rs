3359f341364663eca7e4ea4ee2b10b47eac7eaa7
DrWursterich <31037782+DrWursterich@users.noreply.github.com>
2024-01-15 19:59:24 +0100

    fix: use ? operator over .unwrap()

--- src/project.rs
+++ src/project.rs
@@ -6,11 +6,11 @@ pub(crate) struct WorkingDirectory {
 }
 
 pub(crate) fn get_working_directory(uri: &Url) -> Option<WorkingDirectory> {
-    let path = uri.to_file_path().unwrap().to_str().unwrap().to_owned();
+    let path = uri.to_file_path().ok()?.to_str()?.to_owned();
     // assume directory above "src" is the module
-    let index = path.find("/src/").unwrap();
+    let index = path.find("/src/")?;
     let path = &path[..index];
-    let begin = path.rfind('/').unwrap() + 1;
+    let begin = path.rfind('/')? + 1;
     return Some(WorkingDirectory {
         module: path[begin..].to_string(),
         path: path[..begin].to_string(),
